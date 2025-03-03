100ac3fbe9d0d6e97bccc8009785a55f7ddc4b19
Vaxee03 <012jas84@gmail.com>
2025-03-03 18:59:25 +0900

    [level 2] Title: 구명보트, Time: 11.15 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int light = 0;
+        int heavy = people.length-1;
+        int boats = 0;
+        
+        while (light <= heavy) {
+            if (people[light] + people[heavy] <= limit){
+                light++;;
+            }
+            heavy--;
+            
+            boats++;
+        }
+        
+        return boats;
+    }
+}
