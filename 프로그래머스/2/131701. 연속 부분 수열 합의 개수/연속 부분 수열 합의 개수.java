4d6b822b121aa45f10f19149c0dfb806b82a2a35
Vaxee03 <012jas84@gmail.com>
2025-03-05 20:53:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1752.88 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        int n = elements.length;
+        Set<Integer> uniqueSums = new HashSet<>();
+
+        // 연속 부분 수열의 합 계산
+        for (int length = 1; length <= n; length++) {
+            for (int start = 0; start < n; start++) {
+                int sum = 0;
+                for (int i = 0; i < length; i++) {
+                    sum += elements[(start + i) % n]; // 순환 배열을 직접 처리
+                }
+                uniqueSums.add(sum);
+            }
+        }
+
+        return uniqueSums.size();
     }
 }
